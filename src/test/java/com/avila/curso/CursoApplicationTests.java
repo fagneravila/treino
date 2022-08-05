4db7144e93a7bd1f4aaa7a11b24662ae92859c62
Fagner Avila <fagner.avila@gmail.com>
2022-08-05 01:03:43 -0300

    Project Created

---
+++ src/test/java/com/avila/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avila.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
