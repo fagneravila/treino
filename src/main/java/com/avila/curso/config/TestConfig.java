939b599d75f8591bc738cb1416673e93588663a2
Fagner Avila <fagner.avila@gmail.com>
2022-08-19 02:34:57 -0300

    Categories

--- src/main/java/com/avila/curso/config/TestConfig.java
+++ src/main/java/com/avila/curso/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.avila.curso.config;
 
+import com.avila.curso.entities.Category;
 import com.avila.curso.entities.Order;
 import com.avila.curso.entities.Users;
 import com.avila.curso.entities.enums.OrderStatus;
+import com.avila.curso.repositories.CategoryRepository;
 import com.avila.curso.repositories.OrderRepository;
 import com.avila.curso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Users u1 = new Users(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -34,7 +39,10 @@ public class TestConfig implements CommandLineRunner {
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
-
+        Category c1 = new Category(null,"Informatica");
+        Category c2 = new Category(null,"Limpeza");
+        Category c3 = new Category(null,"Mooveis");
+        categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
 
     }
 }
---
+++ src/main/java/com/avila/curso/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.avila.curso.repositories;
+
+import com.avila.curso.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
