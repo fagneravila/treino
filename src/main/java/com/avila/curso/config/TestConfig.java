521c54e397c6a6776ea4dce8d6cfd8f7ddfd74fc
Fagner Avila <fagner.avila@gmail.com>
2022-08-23 00:29:11 -0300

    Payment one to one association

--- src/main/java/com/avila/curso/config/TestConfig.java
+++ src/main/java/com/avila/curso/config/TestConfig.java
@@ -30,6 +30,8 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
+
+
     @Override
     public void run(String... args) throws Exception {
         Users u1 = new Users(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -71,6 +73,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
 
     }
 }
---
+++ src/main/java/com/avila/curso/entities/Payment.java
@@ -0,0 +1,69 @@
+package com.avila.curso.entities;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant momment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant momment, Order order) {
+        this.id = id;
+        this.momment = momment;
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMomment() {
+        return momment;
+    }
+
+    public void setMomment(Instant momment) {
+        this.momment = momment;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Payment payment)) return false;
+        return Objects.equals(getId(), payment.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
